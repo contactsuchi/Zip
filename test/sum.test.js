67bac276209b2147591c9580cab143207ef2724c
contactsuchi <contactsuchi@yahoo.com>
2026-02-13 16:18:51 -0800

    Added new tests Multiply (#4)
    * Added new tests Multiply
    
    * rebase and main branch rules

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,5 +1,10 @@
-const sum = require("../src/sum");
+const libray = require("../src/sum");
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(libray.sum(1, 2)).toBe(3);
 });
+
+test("multiplies 4 * 2 to equal 8", () => {
+  expect(libray.multiply(4, 2)).toBe(8);
+});
+
